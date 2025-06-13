b8eb245a154c3b60484e0a15f8d648e967a93abf
Luiz Augusto Correa Melo <luiz_gforce@outlook.com>
2025-06-13 07:43:33 -0300

    Estado inicial da tela de login - tem a tela de login e 1 scenepara o cliente

--- src/Controller/Login.java
+++ src/Controller/Login.java
@@ -1,4 +1,51 @@
-package Controller;
+package src.Controller;
+import src.Main;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
 
 public class Login {
+    public Login(){
+
+    }
+    @FXML
+    private Button entrar;
+    @FXML
+    private Label wrongLogin;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+
+    public void userLogin(ActionEvent event) throws IOException {
+        checkLogin();
+    }
+
+    private void checkLogin() throws IOException{
+        Main m = new Main();
+        if(username.getText().toString().equals("Cliente") && password.getText().toString().equals("1234")){
+            wrongLogin.setText("Sucesso");
+
+            m.changeScene("AfterLoginClient.fxml");
+        }
+        if(username.getText().toString().equals("Cliente") && password.getText().toString().equals("1234")){
+            wrongLogin.setText("Sucesso");
+
+            m.changeScene("AfterLoginClient.fxml");
+        }
+    }
+
+
+
 }
